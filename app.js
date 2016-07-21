69160c62af6482f54b66dd677637092de4305f45
s0ph1e <sonya1912@gmail.com>
2016-07-21 19:07:54 +0300

    Create events for clubs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function getClubs(sportlifeScheduleUrl=SPORTLIFE_SCHEDULE_URL) {
 
                     clubs.push({
                         url: clubAbsoluteUrl,
-                        text: `${clubName} (${region})`
+                        title: `${clubName} (${region})`
                     })
                 })
             });
@@ -34,7 +34,7 @@ function filterClubs(foundClubs) {
     function clubIsSelected (foundClub) {
         var neededClubs = CLUBS.map( c => c.toLowerCase());
         return _.some(neededClubs, (neededClub) => {
-            return _.includes(foundClub.text.toLowerCase(), neededClub);
+            return _.includes(foundClub.title.toLowerCase(), neededClub);
         });
     }
 
@@ -43,11 +43,45 @@ function filterClubs(foundClubs) {
     return selectedClubs;
 }
 
+function createEvent(el, date, club) {
+    if (!_.isEmpty(el.text())) {
+        var container = el.find('.event-item-descr');
+        var title = container.find('h4').text();
+        var time = container.find('.time').text();
+        var description = container.find('p').text();
+
+        return { title, time, description, date, club };
+    }
+}
+
 function getScheduleForClub(club) {
     return request(club.url)
         .then(response => {
-            console.log(`SCHEDULE FOR ${club.text}\n`);
-            return club.url;
+            console.log(`SCHEDULE FOR ${club.title}\n`);
+
+            var $ = cheerio.load(response.body, {
+                decodeEntities: false
+            });
+            var table = $('#shedule-content');
+            var tableDates = table.find('thead tr td').not('.time-col');
+            var tableBody = table.find('tbody');
+
+            var clubEvents = tableDates.map( (dateColumnIndex, el) => {
+                var date = $(el).find('span').text();
+                var events = [];
+
+                tableBody.find('tr').not('.time-line').each((hourLineIndex, hourLineElement) => {
+                    $(hourLineElement).find('td').not('.time-col').eq(dateColumnIndex).each((hourCellIndex, hourCellElement) => {
+                        $(hourCellElement).find('.event-item').each((eventItemIndex, eventItemElement) => {
+                            var event = createEvent($(eventItemElement), date, club);
+                            events.push(event)
+                        });
+                    });
+                });
+                return events;
+            }).get();
+
+            return clubEvents;
         })
 }
 
